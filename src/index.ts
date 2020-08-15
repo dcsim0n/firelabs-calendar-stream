574acc9b0aff2463582e94f47889426a7a66f033
Dana Simmons <dsanalog@gmail.com>
2020-08-15 16:21:43 -0400

    Manage duplicate events
    Why: It's possible for the same event to appear several times on the
    calendar. If we fetch the same event multiple times, that is a waste of
    resources.
    
    * ...
    
    Converted the links array into a hash map. so if the event id is already
    in the hash map, just overwrite with the same link, lazy de-dupe
    
    * ...

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {queue} from 'async';
 
 const EVENTS:string = "https://roswellfirelabs.org/events?EventViewMode=1&EventListViewMode=2&SelectedDate=8/15/2020&CalendarViewType=0"
 const EVENT_LINK_SELECTOR:string = ".EventListCalendar a";
-const EVENT_ID_REGEX = /https:\/\/roswellfirelabs\.org\/event-(\d{6})/
+const EVENT_ID_REGEX = /https:\/\/roswellfirelabs\.org\/event-(\d{7})/
 
 interface IEventLink{
   id: number,
@@ -15,25 +15,34 @@ interface IEventLink{
 
 
 
-const CrawlerQueue = queue( (task: IEventLink, cb)=>{
-  axios.get(task.url)
+const CalendarCrawler = queue( (task: IEventLink, cb)=>{
+  const url = `https://roswellfirelabs.org/event-${task.id}/Export`;
+  console.log(`Fetching url: ${url}`)
+  axios.get(url)
   .then((resp)=>{
     console.log(`Fetched data for ${task.id}, length: ${resp.data.length}`)
     cb();
   })
+  .catch( err =>{
+    console.log(`Fetch failed for event id: ${task.id}, ${err.message}`)
+    cb(err)
+  })
 }, 4);
+
+
 
 axios.get(EVENTS)
 .then((resp)=>{
   const events_page = new JSDOM(resp.data)
 
   const link_nodes = <NodeListOf<HTMLAnchorElement>> events_page.window.document.querySelectorAll(EVENT_LINK_SELECTOR);
-  let links: Array<IEventLink> = [];  
+  const links = new Map<number,IEventLink>()
   for (let i = 0; i < link_nodes.length; i++) {
     const eventUrl = link_nodes[i].href;
     const id_str = eventUrl.match(EVENT_ID_REGEX);
     if(id_str){
-      links.push({id: parseInt(id_str[1]), url: eventUrl })
+      const id = parseInt(id_str[1]);
+      links.set(id, {id: id, url: eventUrl });
     }
   }
   console.log(links);
@@ -42,15 +51,17 @@ axios.get(EVENTS)
 .then( links =>{
 
   // for each link
-  CrawlerQueue.push(links)
+  links.forEach(event =>{
+    CalendarCrawler.push(event)
+  })
   
-  console.log(`queue length is: ${CrawlerQueue.length()}`)
+  console.log(`queue length is: ${CalendarCrawler.length()}`)
   // fetch the event page
   // parse the ics link from the event page
   // save and/or download the ics file
   // don't duplicate work
   // maybe should de-dup the links array before getting here
-  return CrawlerQueue.drain(()=>{
+  return CalendarCrawler.drain(()=>{
     console.log("Finished fetching");
   })
 });
