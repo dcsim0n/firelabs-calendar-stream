a0fd28cc6a1ddbbbc6014208293bf55074f6e8e5
Dana Simmons <dsanalog@gmail.com>
2020-08-16 18:42:05 -0400

    Set correct property names
    Why: I just didn't read the module file correctly.
    
    * ...
    
    Created the proper property names of the TicsData type.
    
    * ...

--- api/src/types/ics-to-json.d.ts
+++ api/src/types/ics-to-json.d.ts
@@ -1,15 +1,11 @@
 declare module 'ics-to-json'{
 
   type TIcsData = {
-    EVENT: string,
-    EVENT_START: string,
-    EVENT_END: string,
-    START_DATE: string,
-    DESCRIPTION: string,
-    SUMMARY: string,
-    LOCATION: string,
-    ALARM: string,
-    
+    starDate: string,
+    endDate: string,
+    description: string,
+    summary: string,
+    location: string,
   }
   export default function icsToJson(data:string): TIcsData 
 }
