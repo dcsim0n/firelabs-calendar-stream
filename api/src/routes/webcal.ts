3cd63db2b1dfc0c369786372dee5f730d6780d3c
Dana Simmons <dsanalog@gmail.com>
2020-08-16 18:38:19 -0400

    Bugfix to meet formatting requirements of google calendar
    Why: Google expects a stricter format on ics files than other callendar
    clients
    
    * ...
    
    Added END:VCALENDAR directive to end of data string, this closes the
    srtream of events.
    
    Also found that google callendar requires a url to end in .ics in order
    to be parsed properly.
    
    Some other formatting notes were found here:
    https://support.google.com/calendar/answer/37118?hl=en
    
    * ...

--- api/src/routes/webcal.ts
+++ api/src/routes/webcal.ts
@@ -6,13 +6,20 @@ import icsToJson from 'ics-to-json';
 
 const router = express.Router();
 
-router.get('/api/1/webcal',function(req,resp){
-
-  models.CalendarEvent.findOne({where:{eventId: 3893878}})
-  .then(event =>{
-    if(event){
-      resp.json(icsToJson(event.icsData));
-    }
+router.get('/api/1/events.ics',function(req,resp){
+  let respStr = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Roswell Firelabs//Event Calendar//EN\nMETHOD:PUBLISH\nCALSCALE:GREGORIAN\nX-WR-CALNAME:Events \nX-WR-TIMEZONE:America/New_York\n"
+  models.CalendarEvent.findAll()
+  .then( allEvents =>{
+    allEvents.forEach( event =>{
+      const {icsData} = event;
+      const dataLength = icsData.length
+      const icsWoHeader = event.icsData.slice(46, dataLength-1).slice(0,-14)
+      respStr += icsWoHeader;
+    })
+    respStr += "\nEND:VCALENDAR\n"
+    resp.set('Content-Type', 'text/calendar;charset=utf-8');
+    resp.set('Content-Disposition', 'attachment; filename="firelabs.events.ics"');
+    resp.send(respStr);
   })
 })
 
