9cb3c4d0471d07b3b4c814fe6556670f69a21fa6
Dana Simmons <dana@simmons-audio.com>
2021-04-14 20:13:02 -0400

    Enable ssl for sequelize

--- db/models/index.ts
+++ db/models/index.ts
@@ -10,7 +10,7 @@ import {initCalendarEvent} from './calendarevent'
 import { DbInterface } from '..';
 let sequelize: Sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable]!, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
